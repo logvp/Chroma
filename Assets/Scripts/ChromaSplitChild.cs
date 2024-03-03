13a3694d0ded0ac792c8e5f76de25bee46a2ea71
Log <lrvanpel@asu.edu>
2024-03-03 01:22:57 -0700

    Disable colliders while splitting

--- Assets/Scripts/ChromaSplitChild.cs
+++ Assets/Scripts/ChromaSplitChild.cs
@@ -8,6 +8,8 @@ public class ChromaSplitChild : MonoBehaviour
 
     void OnEnable()
     {
+        Collider collider = GetComponent<Collider>();
+        collider.enabled = false;
         Rigidbody rb = GetComponent<Rigidbody>();
         if (rb != null)
         {
@@ -18,6 +20,8 @@ public class ChromaSplitChild : MonoBehaviour
 
     public void MakeReal()
     {
+        Collider collider = GetComponent<Collider>();
+        collider.enabled = true;
         Rigidbody rb = GetComponent<Rigidbody>();
         if (rb != null)
         {
